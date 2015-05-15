f270b49716aa02d3bba4c7fd3d3ea0cc2d3e6abf
inagaki <inagaki-hiroyuki@mgt-net.com>
2015-05-15 17:20:43 +0900

    Initial Commit

---
+++ gitTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTestApp
+//
+//  Created by user on 2015/05/15.
+//  Copyright (c) 2015年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
